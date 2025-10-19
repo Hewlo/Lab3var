2bdd2304fd1037928d2d23578bdb5870b802df5a
hewlo <hamstersmart1@hotmail.com>
2025-10-19 23:05:12 +0800

    Initial

---
+++ __main__.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    wallet = ['receipt', 5, 0.35, True] #Store String, Integer, Float and Boolean value.
+    print(wallet)
+    print(wallet[0]) #Get item from location 0.
+    print(wallet[3]) #Get item from location 3.
+    print(wallet[1:]) # Print item from Location 1 to the end. (Slice)
+    print(wallet[:3]) # Print item from starting to the end, EXCLUDING the provided value at the right (3).
+
