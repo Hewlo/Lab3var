6d60e530b024e27ccc12f6c02aad624878f1e030
Hewlo <hamstersmart1@hotmail.com>
2025-10-19 23:13:00 +0800

    Added trivia.

--- __main__.py
+++ __main__.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     print(wallet[0]) #Get item from location 0.
     print(wallet[3]) #Get item from location 3.
     print(wallet[1:]) # Print item from Location 1 to the end. (Slice)
-    print(wallet[:3]) # Print item from starting to the end, EXCLUDING the provided value at the right (3).
+    print(wallet[:3]) # Print item from starting to the end, EXCLUDING the provided value at the right (3) (Slice).
 
